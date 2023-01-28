b2e66f559995a3a7e1500c022bbad75bdbb915b5
Ian M. Rodovsky <irodovsk@asu.edu>
2023-01-28 12:16:07 -0800

    Uploading login page HTML, CSS, and JS files. Uploading security API JS file.

---
+++ www/js/login-page.js
@@ -0,0 +1,20 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+let failedLogins = 0;
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+    if (failedLogins === 3) {
+        alert(  'Your account has been locked due to due many failed login attempts.\n' + 
+                'Please contact the customer support team for assistance.');
+    } else if (username === "admin" && password === "password") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+        failedLogins++;
+    }
+})
