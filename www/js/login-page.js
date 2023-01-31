6dfca50eb1d5b575eecb2b3c36e94848d1d482a5
Ian M. Rodovsky <irodovsk@asu.edu>
2023-01-31 15:27:06 -0800

    Updates made.

--- www/js/login-page.js
+++ www/js/login-page.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
@@ -5,14 +7,16 @@ let failedLogins = 0;
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
+
     const username = loginForm.username.value;
     const password = loginForm.password.value;
+    
     if (failedLogins === 3) {
         alert(  'Your account has been locked due to due many failed login attempts.\n' + 
                 'Please contact the customer support team for assistance.');
     } else if (username === "admin" && password === "password") {
         alert("You have successfully logged in.");
-        location.reload();
+        window.location.href = './user-home.html';
     } else {
         loginErrorMsg.style.opacity = 1;
         failedLogins++;
---
+++ www/js/security.js
@@ -0,0 +1,30 @@
+var security = {
+    saltString: function (pString) {
+        let salt = 'BlbKKS2y4r1FeVfjkee0';
+        let saltedString = pString;
+        for (var idx = 0; saltedString.length < 20; idx++)
+            saltedString += salt.charAt(idx);
+        return saltedString;
+    },
+    encryptString: function(pString) {
+        let stringCharInt = '';
+        let modulo = 70952713551039394465955034790943808459480690142438823904128627757210447577079n;
+        let exponent = 65537n;
+        for (var idx = 0; idx < pString.length; idx++) 
+            stringCharInt += pString.charCodeAt(idx).toString();
+        let stringIntEncrypted = (BigInt(stringCharInt) ** exponent) % modulo;
+        return stringIntEncrypted.toString();
+    },
+    
+    hashString: function (pString) {
+        let hash = 1n;
+        let multiplicand = 327970756321988702593190524464565820263n;
+        let modulo = 82586345904536973264402314161588489653348247498903562300518738125739186220113n;
+        for (var idx = 0; idx < pString.length; idx++)
+            hash = (multiplicand * hash + BigInt(pString.charCodeAt(idx))) % modulo;
+        hash = hash.toString(16);
+        while (hash.length < 64)
+            hash = '0' + hash;
+        return hash;
+    }
+};
