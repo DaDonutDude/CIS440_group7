db895fa668185330ba2216698aabf9124a0f12cc
Ian M. Rodovsky <irodovsk@asu.edu>
2023-02-05 18:42:44 -0800

    Updated login to work with database.

--- www/js/login-page.js
+++ www/js/login-page.js
@@ -5,9 +5,20 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
-fetch('./users.json')
-  .then((response) => response.json())
-  .then((json) => users = json);
+$.ajax({
+  type: "POST",
+  url: './php/login-mysql.php',
+  dataType: 'json',
+  data: {functionname: 'getUsers'},
+  success: function (obj, textstatus) {
+                if( !('error' in obj) ) {
+                    users = obj.result;
+                }
+                else {
+                    console.log(obj.error);
+                }
+          }
+});
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
@@ -19,7 +30,7 @@ loginButton.addEventListener("click", (e) => {
     let userFound = false;
 
     while (idx < users.length) {
-      if (users[idx].username.toLowerCase() === username.toLowerCase()) {
+      if (users[idx][0].toLowerCase() === username.toLowerCase()) {
         userFound = true;
         break;
       }
@@ -28,14 +39,23 @@ loginButton.addEventListener("click", (e) => {
 
     if (!userFound) {
       loginErrorMsg.style.opacity = 1;
-    } else if (users[idx].failedAttempts >= 3) {
+    } else if (users[idx][2] >= 3) {
       loginErrorMsg.innerHTML = 'Your account has been locked due to due many failed login attempts. Please contact the customer support team for assistance.';
       loginErrorMsg.style.opacity = 1;
-    } else if (users[idx].username.toLowerCase() === username.toLowerCase() && users[idx].password === password) {
-        if (users[idx].isAdmin === 1) window.location.href = './admin-home.html';
+      $.ajax({
+        type: "POST",
+        url: './php/login-mysql.php',
+        dataType: 'json',
+        data: {functionname: 'lockAccount', arguments: [username]},
+        success: function (obj, textstatus) {
+                      if( ('error' in obj) ) console.log(obj.error);
+                }
+      });
+    } else if (users[idx][0].toLowerCase() === username.toLowerCase() && users[idx][1] === password) {
+        if (users[idx][3] == 1) window.location.href = './admin-home.html';
         else window.location.href = './user-home.html';
     } else {
         loginErrorMsg.style.opacity = 1;
-        users[idx].failedAttempts++;
+        users[idx][2]++;
     }
 })
---
+++ www/php/login-mysql.php
@@ -0,0 +1,31 @@
+<?php
+    header('Content-Type: application/json');
+    $returnVal = array();
+    $userList = array();
+    if( !isset($_POST['functionname']) ) { $returnVal['error'] = 'No function name!'; }
+    if( !isset($returnVal['error']) ) {
+        switch($_POST['functionname']) {
+            case 'getUsers':
+                $conn = new mysqli("107.180.1.16", "springa2023team7", "springa2023team7", "springa2023team7");
+                $result = $conn -> query("SELECT username, password, failedAttempts, isAdmin FROM Users");
+                while ($row = $result -> fetch_row()) {
+                    array_push($userList, [$row[0], $row[1], $row[2], $row[3]]);
+                }
+                $returnVal['result'] = $userList;
+                $conn -> close();
+                echo json_encode($returnVal);
+                break;
+            case 'lockAccount' :
+                $conn = new mysqli("107.180.1.16", "springa2023team7", "springa2023team7", "springa2023team7");
+                $username = $_POST['arguments'][0];
+                $statement = $conn -> prepare("UPDATE Users SET failedAttempts=3 WHERE username=?");
+                $statement -> bind_param('s', $username);
+                $statement -> execute();
+                $conn -> close();
+                break;
+            default:
+                $returnVal['error'] = 'Not found function '.$_POST['functionname'].'!';
+                break;
+        }
+    }
+?>
